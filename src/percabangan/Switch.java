d697d97c4c5df278326005fdcdab1da99299a55f
nur.azizah <azizahn346@gmail.com>
2020-04-15 10:54:49 +0800

    Percabangan Switch

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanan=3;
+        switch (jalanan){
+            case 1:
+                System.out.println("cuci mata");
+                break;
+            case  2:
+                System.out.println("cari udara");
+                break;
+            case 3:
+                System.out.println("beli mamam");
+                break;
+            case 4:
+                System.out.println("bt dirumah");
+                break;
+            default:
+                System.out.println("hantu dkk");
+                break;
+        }
+    }
+}
